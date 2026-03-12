636a019f2d3bb1011721615abd437e014004569d
zoikle <134870626+Gros26@users.noreply.github.com>
2026-03-11 21:42:50 -0500

    modificada la gramatica y agregados nuevos constructores para una implementacion con listas

--- Taller2.rkt
+++ Taller2.rkt
@@ -1,9 +1,111 @@
 #lang eopl
-;; Gramática
+;; Gramática antes
 ; <fnc-exp>  ::= 'FNC <natural> (<and-exp>)
 ; <and-exp>  ::= <clausula> | ('and-exp <clausula> {'or <clausula>}*)
 ; <clausula> ::= <literal> | ('or-exp <literal> {'or <literal>}*)
 ; <literal>  ::= ('literal <entero-no-cero>)
+
+;; Gramática despues
+; <fnc-exp>  ::= ('FNC <natural> (<and-exp>))
+; <and-exp>  ::= <clausula> | (<clausula> {'and <clausula>}+)
+; <clausula> ::= <literal> | (<literal> {'or <literal>}+)
+; <literal>  ::= <entero-no-cero>
+
+;______________________________________;
+;Gramatica actualll
+; <fnc-exp>  ::=  ('FNC <natural> <and-exp>)
+; <and-exp>  ::=  <clausula>
+;            ::=  (<clausula> 'and <and-exp>)
+; <clausula> ::=  (<or-exp>)
+; <or-exp>   ::=  <literal>
+;            ::=  <literal> 'or <or-exp>
+; <literal>  ::=  <entero-no-cero>
+
+;intenta representar esto
+; (FNC 1 (1)) 
+; (FNC 2 (1 or 2 or -1))
+
+; (FNC 2 ((1 or 2 or -1) and (2 or 1 or 2)))
+
+
+
+; Definamos la interfaz, nuestro contrato
+; Constructores
+; fnc-exp: entero-no-cero x <and-exp> -> fnc-exp
+; and-simple: clausula -> and-exp
+; and-compuesto: clausula x and-exp -> and-exp
+; clausula-exp: or-exp -> clausula
+; or-simple: literal -> or-exp
+; or-compuesto: literal x or-exp -> or-exp
+; lit-exp: entero-no-cero -> literal
+
+
+(define lit-exp
+  (lambda (number)
+    (list 'literal number)
+    )
+  )
+
+(define or-simple
+  (lambda (literal)
+    (list 'or-simple literal)
+    )
+  )
+
+(define or-compuesto
+  (lambda (literal or-exp)
+    (list 'or-compuesto literal or-exp)))
+
+; usos 
+(or-compuesto (lit-exp 2) 
+               (or-compuesto (lit-exp 1) 
+                             (or-simple (lit-exp 2))))
+(or-simple (lit-exp 2))
+
+(or-compuesto (lit-exp 2) (lit-exp 2))
+;------
+
+(define clausula-exp
+  (lambda (or-exp)
+     (list 'clausula-exp or-exp)
+    )
+  )
+
+(clausula-exp (or-simple (lit-exp 2)))
+
+(define and-simple
+  (lambda (clausula)
+    (list 'and-simple clausula)
+    )
+  )
+
+(and-simple (clausula-exp (or-simple (lit-exp 2))))
+
+
+(define and-compuesto
+  (lambda (clausula and-exp)
+    (list 'and-compuesto clausula and-exp)
+    )
+  )
+
+(and-compuesto (clausula-exp (or-simple (lit-exp 2))) (and-simple (clausula-exp (or-simple (lit-exp 2))))
+)
+
+(define fnc-exp
+  (lambda (entero-no-cero and-exp)
+    (list 'FNC entero-no-cero and-exp)
+    )
+  )
+
+(fnc-exp 3 (and-compuesto (clausula-exp (or-simple (lit-exp 2))) (and-simple (clausula-exp (or-simple (lit-exp 2))))
+))
+(fnc-exp 2 (and-simple (clausula-exp (or-simple (lit-exp 2)))))
+(fnc-exp 2 
+  (and-compuesto 
+    (clausula-exp (or-compuesto (literal 1) (or-simple (literal 2))))
+    (and-simple (clausula-exp (or-simple (literal -1))))))
+
+
 
 
 ;;;; Implementación Basada en Listas ;;;;
@@ -56,11 +158,11 @@
 
 ;; Constructor de fnc
 ; and-exp : estrctura and 
-(define fnc-exp
-  (lambda (and-exp)
-    (list 'FNC count-vars(and-exp) and-exp)
-    )
-  )
+;(define fnc-exp
+;  (lambda (and-exp)
+ ;   (list 'FNC count-vars(and-exp) and-exp)
+;    )
+ ; )
 
 ;; Funciones auxiliares
 
